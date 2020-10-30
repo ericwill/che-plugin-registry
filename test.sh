8a6ba0a395410075a3fbe29272bfb3ad3917c927
Eric Williams <ericwill@redhat.com>
2020-10-30 14:47:39 -0400

    Add scala test dir
    Signed-off-by: Eric Williams <ericwill@redhat.com>

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2018-2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+FILES_CHANGED=($(git diff --name-only -r sidecar_github_actions))
+
+for i in "${FILES_CHANGED[@]}"
+do
+    if [[ $i == 'sidecars/'* ]]; then
+        echo $i
+    fi
+done
